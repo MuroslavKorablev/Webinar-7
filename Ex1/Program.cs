17ff88d24451f8a54b3bea90ff118bc8077b6826
Kleo <mmttdd16@gmail.com>
2023-10-01 13:53:46 +0300

    Finished Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string path = "C:/Users/Мирослав/Documents/Manga";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
